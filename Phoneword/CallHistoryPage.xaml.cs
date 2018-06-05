2e6b372546b660360557b5d448eac732a2bcd0b7
arrongourlaycts <supern0va>
2018-06-05 10:37:08 +0100

    Initial check-in of module Phoneword

---
+++ Phoneword/CallHistoryPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class CallHistoryPage : ContentPage
+    {
+        public CallHistoryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCall(object sender, EventArgs e)
+        {
+            var dialer = DependencyService.Get<IDialer>();
+                dialer.Dial("555555555");
+
+        }
+    }
+}
